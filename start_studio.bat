897f30edf0c6b54d0161e18f130d5a95db318fc2
pnlokeswari <pnlokeswari@gmail.com>
2026-07-31 20:14:53 +0530

    Migrating start_studio.bat to cloud

---
+++ start_studio.bat
@@ -0,0 +1,8 @@
+@echo off
+title Zero483 Pinterest Blog Studio Launcher
+echo ========================================================
+echo  Launching Zero483 Pinterest Blog Studio...
+echo ========================================================
+cd /d "%~dp0"
+C:\Users\Ram\AppData\Local\Python\bin\python.exe admin_server.py
+pause
